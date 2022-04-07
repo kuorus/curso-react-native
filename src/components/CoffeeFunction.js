8248f40a0a9876641e8e91ad49e5972545521e82
Francisco A. Vargas <contacto@franciscovargas.es>
2022-04-07 19:14:15 +0200

    Conditional rendering

--- src/components/CoffeeFunction.js
+++ src/components/CoffeeFunction.js
@@ -1,10 +1,32 @@
 import { Text, View } from 'react-native'
 import React from 'react'
 
-const CoffeeFunction = props => (
-  <View>
-    <Text>{props.cafe.name}</Text>
-  </View>
-)
+const CoffeeFunction = ({ cafe }) => {
+  let cuteName = cafe.name
+  if (cafe.name === 'Espresso') {
+    cuteName = 'Espresso in tazza'
+  }
+
+  return (
+    <View>
+      <Text>{cuteName}</Text>
+    </View>
+  )
+
+  /*
+  return (
+    <View>
+      <Text>{cafe.name === 'Espresso' ? "Espresso in tazza" : cafe.name}</Text>
+    </View>
+  )
+*/
+
+  /*  return (
+    <View>
+      {cafe.name === 'Espresso' && <Text>Espresso in tazza</Text>}
+      {cafe.name !== 'Espresso' && <Text>{cafe.name}</Text>}
+    </View>
+  )*/
+}
 
 export default CoffeeFunction
